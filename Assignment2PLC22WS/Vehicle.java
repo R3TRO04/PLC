81c7ff187f3006696222130423f1daa9ca092a58
Nico Beranek <nico@jube.at>
2022-10-20 21:07:06 +0200

    update Abgabe 2

--- Assignment2PLC22WS/Vehicle.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Nico Beranek
- * @id 11807137
- * @comment READ-ONLY FILE 
- */
-
-public interface Vehicle {
-    int getId();
-}
--- Assignment2PLC22WS/src/TrafficController.java
+++ Assignment2PLC22WS/src/TrafficController.java
@@ -1,11 +1,5 @@
-/**
- * @author Nico Beranek
- * @id 11807137
- * @comment READ-ONLY FILE
- */
-
 public interface TrafficController {
-    void enterRight(Vehicle v); // only returns if bridge is empty; blocks otherwise
+	void enterRight(Vehicle v); // only returns if bridge is empty; blocks otherwise
     void enterLeft(Vehicle v);  // only returns if bridge is empty; blocks otherwise
     void leaveLeft(Vehicle v);  // must be called after enterRight
     void leaveRight(Vehicle v); // must be called after enterLeft
