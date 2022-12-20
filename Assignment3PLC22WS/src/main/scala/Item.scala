57722a0e9b8e658dcb72899d12c2f573961c5e01
R3TRO <nico@jube.at>
2022-12-20 22:31:29 +0100

    updated project

--- Assignment3PLC22WS/src/main/scala/Item.scala
+++ Assignment3PLC22WS/src/main/scala/Item.scala
@@ -1,10 +1,5 @@
 trait Item {
-  var id: Int
-  var name: String
-  var value: Int
-
-  def getID: Int = {
-    id += 1
-    id
-  }
+  val id: Int
+  val name: String
+  val value: Int
 }
--- Assignment3PLC22WS/src/main/scala/StoreItem.scala
+++ Assignment3PLC22WS/src/main/scala/StoreItem.scala
@@ -1,3 +1,3 @@
-class StoreItem(val id: Int, val name: String, val value: Int) extends Item with Logger{
+class StoreItem(override val id: Int,override val name: String,override val value: Int) extends Item with Logger{
     override def logAction(actionName: String, name: String): Unit = println(s"$name $actionName.")
 }
